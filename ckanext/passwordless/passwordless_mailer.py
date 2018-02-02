3f51241f345fd7f53b74a037eaf1b6ebe345bc8e
espona <lucia.espona@gmail.com>
2018-02-02 12:29:07 +0100

    new mail templates

---
+++ ckanext/passwordless/passwordless_mailer.py
@@ -0,0 +1,40 @@
+# encoding: utf-8
+from ckanext.passwordless import util 
+
+import ckan.lib.mailer as mailer
+from ckan.lib.base import render_jinja2
+from ckan.common import config
+#import ckan.lib.helpers as h
+import ckan.plugins.toolkit as toolkit
+
+import logging
+log = logging.getLogger(__name__)
+
+def passwordless_send_reset_link(user):
+    log.debug("passwordless_send_reset_link user = " + str(user))
+    mailer.create_reset_key(user)
+    body = passwordless_get_reset_link_body(user)
+    extra_vars = {
+        'site_title': config.get('ckan.site_title')
+    }
+    subject = render_jinja2('emails/reset_password_subject.txt', extra_vars)
+
+    # Make sure we only use the first line
+    subject = subject.split('\n')[0]
+
+    mailer.mail_user(user, subject, body)
+
+def passwordless_get_reset_link_body(user):
+    login_link = toolkit.url_for(controller='user', action='login', qualified=True)
+    reset_link = mailer.get_reset_link(user)
+    extra_vars = {
+        'login_link':login_link,
+        'reset_link': reset_link,
+        'site_title': config.get('ckan.site_title'),
+        'site_url': config.get('ckan.site_url'),
+        'user_name': user.name,
+        'user_email': user.email,
+        'key':util.get_key_from_link(reset_link)
+        }
+    # NOTE: This template is translated
+    return render_jinja2('emails/reset_password.txt', extra_vars)
