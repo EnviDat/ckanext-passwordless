f91bcf1e9e13291d9171a68e944a23f9e160ae1b
Lucia Espona Pernas <espona@users.noreply.github.com>
2018-08-20 00:32:01 +0200

    Update passwordless_mailer.py
    Try to get the current token in the logs

--- ckanext/passwordless/passwordless_mailer.py
+++ ckanext/passwordless/passwordless_mailer.py
@@ -10,8 +10,8 @@ import logging
 log = logging.getLogger(__name__)
 
 def passwordless_send_reset_link(user):
-    log.debug("passwordless_send_reset_link user = " + str(user))
     mailer.create_reset_key(user)
+    log.debug("passwordless_send_reset_link user = " + str(user))
     body = passwordless_get_reset_link_body(user)
     extra_vars = {
         'site_title': config.get('ckan.site_title')
